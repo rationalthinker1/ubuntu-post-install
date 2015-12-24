adccb747372f2517c3578b9316d9e535f14ce8d0
Raza Farooq <razaf88@gmail.com>
2015-12-24 02:59:53 -0500

    - Moved all application scripts in applications folder

--- bin/functions.sh
+++ bin/functions.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-function hello() {
-	NAME=$1
-
-	echo -e "Hello ${NAME}! How are you?"
-}
-
 function install() {
 	ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
 	BASE_DIR=$(dirname ${ABSOLUTE_PATH})
 	FILENAME=$1
 
-	${BASE_DIR}/${FILENAME}.sh
+	${BASE_DIR}/applications/${FILENAME}.sh
 }
 
 function apt-install() {

dab544a1402545f8fc6852516bbf9815585a482d
Raza Farooq <razaf88@gmail.com>
2015-12-24 02:39:35 -0500

    - Added main installation script: install.sh - Added function install() in ./bin/functions.sh - Added check to see root is running

--- bin/atom.sh
+++ bin/atom.sh
@@ -1,19 +1,6 @@
 #!/bin/bash 
 echo "Installing Atom..."
 
-# Add repository
-add-apt-repository ppa:unit193/encryption
-
-# Update list of available packages
-apt-get -y update
-
-# Install
-apt-get install -y --no-install-recommends veracrypt
-
-
-#!/bin/bash
-echo "Installing Atom..."
-
 # Download installation file
 wget https://atom.io/download/deb -O /tmp/atom-deb
 
---
+++ bin/functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function install() {
+	ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
+	BASE_DIR=$(dirname ${ABSOLUTE_PATH})
+
+	FILENAME=$1
+	${BASE_DIR}/${FILENAME}.sh
+}
+
+function hello() {
+	NAME=$1
+
+	echo -e "Hello ${NAME}! How are you?"
+}
